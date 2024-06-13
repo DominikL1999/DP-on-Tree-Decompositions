da696af2f39c2e4a11d39987f6230bafcc3d1f4c
Dominik <dominik.leko3@gmail.com>
2024-06-13 11:28:48 +0200

    Refactor tests for stringSplit.

--- src/header/util.h
+++ src/header/util.h
@@ -2,5 +2,54 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 std::vector<std::string> stringSplit(const std::string& str, char delim);
+
+//// Unit testing utilities ////
+
+template<typename T>
+bool returnAndOutputOnFailure(const T& expected, const T& got) {
+    bool success = expected == got;
+    if (!success)
+        std::cout << "Expected: " << expected << ". Got: " << got << std::endl;
+    return success;
+}
+
+//// Pretty printing ////
+
+// Try it out: If you have a local object `p` of type std::pair<T1,T2>, call:
+// `std::cout << p << << std::endl;`
+template<typename T1, typename T2>
+std::ostream& operator<<(std::ostream& stream, const std::pair<T1,T2>& pair) {
+    return stream << "(" << pair.first << "," << pair.second << ")";
+}
+
+// Try it out: If you have a local object `v` of type std::vector<T>, call:
+// `std::cout << v << << std::endl;`
+template<typename T>
+std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vec) {
+    stream << "[";
+    for (size_t i = 0; i < vec.size(); i++) {
+        stream << vec[i];
+        if (i < vec.size() - 1)
+            stream << ",";
+    }
+    stream << "]";
+    return stream;
+}
+
+// Try it out: If you have a local object `m` of type std::unordered_map<T1,T2, call:
+// `std::cout << m << << std::endl;`
+template<typename T1, typename T2>
+std::ostream& operator<<(std::ostream& stream, const std::unordered_map<T1,T2>& map) {
+    int count = 0;
+    for (auto it = map.begin(); it != map.end(); it++) {
+        const std::pair<T1,T2>& pair = *it;
+        stream << pair.first << " -> " << pair.second;
+        if (count < map.size() - 1)
+            stream << " | ";
+        count++;
+    }
+    return stream;
+}
--- test/util/test_string_split.cpp
+++ test/util/test_string_split.cpp
@@ -1,16 +1,21 @@
 #include "util.h"
 
-// Test 1. Return true on success
+#include <iostream>
+
 bool test_string_split_1() {
     std::string str = "Hello, world, how are you doing, today?";
     std::vector<std::string> parts = stringSplit(str, ',');
-    return parts.size() == 4 &&
-        parts[0] == "Hello" &&
-        parts[1] == " world" &&
-        parts[2] == " how are you doing" &&
-        parts[3] == " today?";
+    return returnAndOutputOnFailure(std::vector<std::string>{"Hello"," world"," how are you doing", " today?"}, parts);
+}
+
+bool test_string_split_2() {
+    std::string str = "a,,20";
+    std::vector<std::string> parts = stringSplit(str, ',');
+    return returnAndOutputOnFailure(std::vector<std::string>{"a","","20"}, parts);
 }
 
 int test_string_split(int argc, char** argv) {
-    return !test_string_split_1();
+    return !(
+        test_string_split_1() &&
+        test_string_split_2());
 }
